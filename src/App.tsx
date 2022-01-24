efa55b154951c43b9d5e7eca2b870560167868e4
Brofix <9402720+Blatts12@users.noreply.github.com>
2022-01-24 14:06:45 +0100

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import { AppContainer, Container } from "./components/common/Container";
 import Navbar from "./components/navigation/Navbar";
 import globalStyles from "./globalStyles";
+import Home from "./pages/Home";
 import useUiStore from "./stores/UiStore";
 
 const getIsMobile = () => window.innerWidth <= 600;
@@ -25,7 +27,11 @@ const App: React.FC = () => {
     <>
       <AppContainer>
         <Navbar />
-        <Container></Container>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
       </AppContainer>
     </>
   );
